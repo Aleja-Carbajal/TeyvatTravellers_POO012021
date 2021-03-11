a086a8c02f45e1b06fc359b39a6e76d4d4657472
Aleja-Carbajal <00253720@uca.edu.sv>
2021-03-10 22:12:42 -0600

    First Commit

---
+++ Program/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+
+            Console.WriteLine("Ingrese un valor para la variable X:");
+            x = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un valor para la variable Y:");
+            y = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"La suma de {x} + {y} es igual a {AddNumbers(x,y)}");
+
+        }
+
+        public static int AddNumbers(int x, int y)
+        {
+            return x+y;
+        }
+    }
+}
